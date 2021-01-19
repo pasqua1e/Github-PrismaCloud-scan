9f426c77f551b68ae5246bcd300252bc42736e1a
pasqua1e <31853954+pasqua1e@users.noreply.github.com>
2021-01-19 17:43:19 +0000

    Update test.tf

--- test.tf
+++ test.tf
@@ -2,3 +2,12 @@ resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
 }
+resource "aws_iam_account_password_policy" "strict" {
+  minimum_password_length        = 8
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = true
+  allow_users_to_change_password = true
+  max_password_age               = 80
+}

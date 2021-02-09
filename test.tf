baf79c40c88c0237d863598fccbe3d36c3a1055d
pasqua1e <31853954+pasqua1e@users.noreply.github.com>
2021-02-09 18:32:04 +0000

    Update test.tf

--- test.tf
+++ test.tf
@@ -10,3 +10,11 @@ resource "aws_iam_account_password_policy" "strict" {
   allow_users_to_change_password = true
   max_password_age               = 91
 }
+module "web_server_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+  name        = "web-server"
+  description = "Security group for web-server with HTTP ports open within VPC"
+  vpc_id      = "vpc-12345678"
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+}

11130b49e1a64e874427e7944318d8b0159c41e7
pasqua1e <31853954+pasqua1e@users.noreply.github.com>
2021-02-09 18:35:52 +0000

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,15 +1,6 @@
 provider "aws" {
     region = "eu-west-2"
 }
-resource "aws_iam_account_password_policy" "strict" {
-  minimum_password_length        = 15
-  require_lowercase_characters   = true
-  require_numbers                = true
-  require_uppercase_characters   = true
-  require_symbols                = true
-  allow_users_to_change_password = true
-  max_password_age               = 91
-}
 module "web_server_sg" {
   source = "terraform-aws-modules/security-group/aws//modules/http-80"
   name        = "web-server"

54d6aa7cb4c3474c174a26d01ec9770f27d702e5
pasqua1e <31853954+pasqua1e@users.noreply.github.com>
2021-02-19 18:25:12 +0000

    Update test.tf
    dd

--- test.tf
+++ test.tf
@@ -5,7 +5,7 @@ module "web_server_sg" {
   source = "terraform-aws-modules/security-group/aws//modules/http-80"
   name        = "web-server"
   description = "Security group for web-server with HTTP ports open within VPC"
-  vpc_id      = "vpc-123456789"
+  vpc_id      = "vpc-123456788"
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
 }
